ef6162b864ee4dff15b7b418f62015c4acee42ca
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-02-15 14:18:57 -0800

    removed thrawn

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -3,11 +3,14 @@ Tasks: Open an img and get pixel data
        Figure out how to play sound
        Find an algorithm to convert pixels to sound
 */
+#include "pixel.hpp"
+
+#pragma comment(lib, "winmm.lib")
 
 int main(void) {
     
-    
-    
+    //PlaySound((LPCSTR)"C:\\kenny g.WAV", NULL, SND_ASYNC);
+    PlaySound(TEXT("SystemStart"), NULL, SND_ALIAS);
     
     
     
