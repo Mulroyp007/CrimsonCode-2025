fed8a2c51ac2bf7e37fa7c7ca03e01d35f50db38
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-02-15 13:48:40 -0800

    files

---
+++ Project/Project/main.cpp
@@ -0,0 +1,15 @@
+/*
+Tasks: Open an img and get pixel data
+       Figure out how to play sound
+       Find an algorithm to convert pixels to sound
+*/
+
+int main(void) {
+    
+    
+    
+    
+    
+    
+    return 0;
+}
