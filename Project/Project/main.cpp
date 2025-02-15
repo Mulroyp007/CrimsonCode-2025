9dfc8365686292c11648b2cd2060424e7c37570d
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-02-15 15:38:50 -0800

    guitar noise

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -3,11 +3,14 @@ Tasks: Open an img and get pixel data
        Figure out how to play sound
        Find an algorithm to convert pixels to sound
 */
+#include "pixel.hpp"
+
+#pragma comment(lib,"winmm.lib")
 
 int main(void) {
     
     
-    
+    PlaySound(TEXT("Note_block_guitar.ogg.wav"), NULL, SND_FILENAME);
     
     
     
