cce44d7445b93eef365880fd2a119355b50725ea
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-02-15 16:49:40 -0800

    Getters
    wrote some simple getters

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -16,15 +16,6 @@ int main(void) {
     
     PNGImage image("thrawn6.png");  
     
-    std::srand(std::time(0));
-    int random = 0;
-    random = std::rand() % 10 + 1;
-    int guess = 0;
-
-    std::cout << "Guess a number 1-10: ";
-    std::cin >> guess;
-    if (guess == random) std::cout << "Correct Guess!!!!!" << std::endl;
-    else std::cout << "You stoopid" << std::endl;
     
     return 0;
 }
